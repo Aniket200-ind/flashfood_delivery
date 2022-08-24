31280e43a43e399554ddeb6bc5f251d4d3147e94
Aniket Botre <aniketbotre007@gmail.com>
2022-08-24 22:12:16 +0530

    Built a food delivery clone

---
+++ js/script.js
@@ -0,0 +1,14 @@
+window.onload = function(){ document.getElementById("loading").style.display = "none" };
+
+const hamburger = document.querySelector(".hamburger");
+const navMenu = document.querySelector(".nav-menu");
+
+hamburger.addEventListener("click", () => {
+    hamburger.classList.toggle("active");
+    navMenu.classList.toggle("active");
+});
+
+document.querySelectorAll(".nav-items").forEach(n => n.addEventListener("click", ()=>{
+    hamburger.classList.remove("active");
+    navMenu.classList.toggle("active");
+}));
